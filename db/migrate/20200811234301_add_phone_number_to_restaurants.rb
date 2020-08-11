41659bca6a872f79edbe55f25d609999c6ddc6c1
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-08-11 19:58:49 -0400

    add review model

--- db/migrate/20200811234301_add_phone_number_to_restaurants.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneNumberToRestaurants < ActiveRecord::Migration[6.0]
-  def change
-    add_column :restaurants, :phone_number, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_234301) do
+ActiveRecord::Schema.define(version: 2020_08_11_235731) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "category"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
