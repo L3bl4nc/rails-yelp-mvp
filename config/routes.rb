a62ec20beeda58c9f35732e3f3c58604658f2774
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-08-11 21:36:45 -0400

    controller and view for index and show done

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'restaurants' => 'restaurants#index'
+
+  get 'restaurants/new' => 'restaurants#new'
+  post 'restaurants' => 'restaurants#create'
+
+  get 'restaurants/:id' => 'restaurants#show', as: :restaurant
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up data base"
+Restaurant.destroy_all
+puts "My database is clean"
+
+puts "Creating restaurants"
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  puts "Restaurant #{restaurant.id} is created"
+end
+
+puts "Done!"
